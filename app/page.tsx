9118b1ff9d5cd4afef5729d56ac571a628d9fdc6
Horikaze <adrianyt659@gmail.com>
2024-04-28 01:42:46 +0200

    added hitgub link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { FaGithub } from "react-icons/fa";
 import Board from "./components/Board";
 
 export default function Home() {
   return (
     <main className="size-full">
       <Board />
+      <a
+        href="https://github.com/Horikaze/yet-another-todo-app"
+        className="fixed bottom-5 right-5"
+        target="_blank"
+      >
+        <FaGithub className="size-10" />
+      </a>
     </main>
   );
 }
